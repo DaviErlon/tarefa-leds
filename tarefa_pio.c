29aa7447ab297fcc8b61870b70b919aaf66f7c43
DaviErlon <163437822+DaviErlon@users.noreply.github.com>
2025-01-24 21:32:12 -0300

    adicionei as funções para o keypad

--- tarefa_pio.c
+++ tarefa_pio.c
@@ -5,21 +5,69 @@
 
 #include "ws2812b.pio.h"
 
+#define ROWS 4
+#define COLS 4
 #define LED_COUNT 25
 #define LED_PIN 7
 
+const uint8_t ROW_PINS[ROWS] = {8, 9, 6, 5}; // R1 R2 R3 R4
+const uint8_t COL_PINS[COLS] = {4, 3, 2, 27}; // C1 C2 C3 C4
+
+// Mapeamento do teclado matricial
+const char KEY_MAP[ROWS][COLS] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}
+};
+
+// Inicialização da GPIOs utilizadas pelo KeyPad
+void keypad_init() {
+    for (int i = 0; i < ROWS; i++) {
+        gpio_init(ROW_PINS[i]);
+        gpio_set_dir(ROW_PINS[i], GPIO_OUT);
+        gpio_put(ROW_PINS[i], 0);
+    }
+    for (int i = 0; i < COLS; i++) {
+        gpio_init(COL_PINS[i]);
+        gpio_set_dir(COL_PINS[i], GPIO_IN);
+        gpio_pull_down(COL_PINS[i]);
+    }
+}
+
+// Leitura do Keypad
+char read_keypad() {
+    for (int row = 0; row < ROWS; row++) {
+        gpio_put(ROW_PINS[row], 1);
+
+        for (int col = 0; col < COLS; col++) {
+            if (gpio_get(COL_PINS[col])) {
+                gpio_put(ROW_PINS[row], 0);
+                sleep_ms(20);
+                return KEY_MAP[row][col];
+            }
+        }
+        gpio_put(ROW_PINS[row], 0);
+    }
+    return '\0';
+}
+
+// Estrutura com os dados de cor e luminozidade para um led
 typedef struct{
     uint8_t R;
     uint8_t G;
     uint8_t B;
 } led;
 
-led matriz_led[LED_COUNT];
+// Array que armazena os dados dos 25 leds
+led matriz_led[LED_COUNT]; 
 
+// Função que junta 3 bytes com informações de cores e mais 1 byte vazio para temporização
 uint32_t valor_rgb(uint8_t B, uint8_t R, uint8_t G){
   return (G << 24) | (R << 16) | (B << 8);
 }
 
+// Função que configura um led específico
 void set_led(uint8_t indice, uint8_t r, uint8_t g, uint8_t b){
     if(indice < 25){
     matriz_led[indice].R = r;
@@ -28,6 +76,7 @@ void set_led(uint8_t indice, uint8_t r, uint8_t g, uint8_t b){
     }
 }
 
+// Função de limpar o array de leds
 void clear_leds(){
     for(uint8_t i = 0; i > LED_COUNT; i++){
         matriz_led[i].R = 0;
@@ -36,6 +85,7 @@ void clear_leds(){
     }
 }
 
+// Função que envia os dados do array para os leds via PIO
 void print_leds(PIO pio, uint sm){
     uint32_t valor;
     for(uint8_t i = 0; i < LED_COUNT; i++){
@@ -53,22 +103,51 @@ int main(){
     double r = 0.0, b = 0.0 , g = 0.0;
     ok = set_sys_clock_khz(128000, false);
 
-    //
+    // Inicializações básicas
 
     stdio_init_all();
+    keypad_init();
 
     //configurações da PIO
     uint offset = pio_add_program(pio, &ws2812b_program);
     uint sm = pio_claim_unused_sm(pio, true);
     ws2812b_program_init(pio, sm, offset, LED_PIN);
 
-    clear_leds();
-
-    set_led(3, 100, 100, 100);
-    
-    print_leds(pio, sm);
 
     while (true) {
-        sleep_ms(100);
+        char key = read_keypad();
+
+        if (key != '\0') {
+            switch (key) {
+                case '0':
+
+                    break;
+                case '1':
+
+                    break;
+                case '2':
+
+                    break;
+                case 'A':
+
+                    break;
+                case 'B':
+
+                    break;
+                case 'C':
+
+                    break;
+                case 'D':
+  
+                    break;
+                case '#':
+
+                    break;
+                case '*':
+ 
+                    break;  
+            }
+        }
+        sleep_ms(20);
     }
 }
